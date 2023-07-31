fee35a5fa61d7fc2fd083f0f9c6db6a147f38709
Satyanarayan Dalei <satyanarayandalei65@gmail.com>
2023-07-31 14:09:20 +0530

    adding link tree page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = (props) => {
             <p style={{ color: `${props.mode === `dark` ? `black` : `white`}` }} className='text-center my-3' >Hi My name is Satyanarayan & I Hope u like this app.
                 <p>Visit :
                     <Link target='_blank' to="https://github.com/satyadalei/textutils"> <i className="fab fa-github"></i> Github Link</Link>  & 
-                    <Link target='_blank' to="https://satyadalei.github.io/portfolio/">Portfolio&nbsp;<i class="fa fa-external-link"></i></Link> or 
+                    <Link target='_blank' to="https://linktr.ee/satya_it">Portfolio&nbsp;<i class="fa fa-external-link"></i></Link> or 
                     <Link target='_blank' to={"https://www.linkedin.com/in/satyadalei/"} > <i class="fab fa-linkedin"></i> LinkedIn </Link>
                 </p>
             </p>
