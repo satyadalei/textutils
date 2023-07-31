ecd076d3d14cc281177bb119a73154d195e2908f
Satyanarayan Dalei <satyanarayandalei65@gmail.com>
2023-07-31 14:02:31 +0530

    Adding all links redirect to new page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ const Footer = (props) => {
         <div style={{ backgroundColor: props.mode === `dark` ? `white` : `black`, minHeight: "15vh", marginTop: "1rem", paddingTop: '20px' }} >
             <p style={{ color: `${props.mode === `dark` ? `black` : `white`}` }} className='text-center my-3' >Hi My name is Satyanarayan & I Hope u like this app.
                 <p>Visit :
-                    <Link to="https://github.com/satyadalei/textutils"> <i className="fab fa-github"></i> Github Link</Link>  & 
-                    <Link to="https://satyadalei.github.io/portfolio/">Portfolio&nbsp;<i class="fa fa-external-link"></i></Link> or 
-                    <Link to={"https://www.linkedin.com/in/satyadalei/"} > <i class="fab fa-linkedin"></i> LinkedIn </Link>
+                    <Link target='_blank' to="https://github.com/satyadalei/textutils"> <i className="fab fa-github"></i> Github Link</Link>  & 
+                    <Link target='_blank' to="https://satyadalei.github.io/portfolio/">Portfolio&nbsp;<i class="fa fa-external-link"></i></Link> or 
+                    <Link target='_blank' to={"https://www.linkedin.com/in/satyadalei/"} > <i class="fab fa-linkedin"></i> LinkedIn </Link>
                 </p>
             </p>
         </div>
